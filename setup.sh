bfebcbce2ffa9d36dbe98af2fbb541bfb3376352
Peter Holzer <peter.holzer89@gmail.com>
2021-04-03 12:12:57 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~
+
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar zxvf bcm2835-1.60.tar.gz 
+cd bcm2835-1.60/
+sudo ./configure
+sudo make
+sudo make check
+sudo make install
+cd ..
+
+sudo apt-get install wiringpi
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+gpio -v
+
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
---
+++ src/main.py
@@ -0,0 +1,61 @@
+import sys
+import os
+resourcesdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'resources')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5b_HD
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5b_HD Demo")
+
+    epd = epd7in5b_HD.EPD()
+
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+
+    font24 = ImageFont.truetype(os.path.join(resourcesdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(resourcesdir, 'Font.ttc'), 18)
+
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    Other = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw_Himage = ImageDraw.Draw(Himage)
+    draw_other = ImageDraw.Draw(Other)
+    draw_Himage.text((10, 0), 'hello world', font = font24, fill = 0)
+    draw_Himage.text((10, 20), '7.5inch e-Paper', font = font24, fill = 0)
+    draw_Himage.text((150, 0), u'微雪电子', font = font24, fill = 0)    
+    draw_other.line((20, 50, 70, 100), fill = 0)
+    draw_other.line((70, 50, 20, 100), fill = 0)
+    draw_other.rectangle((20, 50, 70, 100), outline = 0)
+    draw_other.line((165, 50, 165, 100), fill = 0)
+    draw_Himage.line((140, 75, 190, 75), fill = 0)
+    draw_Himage.arc((140, 50, 190, 100), 0, 360, fill = 0)
+    draw_Himage.rectangle((80, 50, 130, 100), fill = 0)
+    draw_Himage.chord((200, 50, 250, 100), 0, 360, fill = 0)
+    epd.display(epd.getbuffer(Himage),epd.getbuffer(Other))
+    time.sleep(2)
+
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear()
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5b_HD.epdconfig.module_exit()
+    exit()
