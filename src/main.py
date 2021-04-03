33edbaa432467b743c74b1b37de36c6a8cb7103d
Peter Holzer <peter.holzer89@gmail.com>
2021-04-03 12:34:07 +0200

    changing displayed content

--- src/main.py
+++ src/main.py
@@ -30,21 +30,14 @@ try:
     logging.info("1.Drawing on the Horizontal image...")
     Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     Other = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw_Himage = ImageDraw.Draw(Himage)
+
     draw_other = ImageDraw.Draw(Other)
-    draw_Himage.text((10, 0), 'hello world', font = font24, fill = 0)
-    draw_Himage.text((10, 20), '7.5inch e-Paper', font = font24, fill = 0)
-    draw_Himage.text((150, 0), u'微雪电子', font = font24, fill = 0)    
-    draw_other.line((20, 50, 70, 100), fill = 0)
-    draw_other.line((70, 50, 20, 100), fill = 0)
-    draw_other.rectangle((20, 50, 70, 100), outline = 0)
-    draw_other.line((165, 50, 165, 100), fill = 0)
-    draw_Himage.line((140, 75, 190, 75), fill = 0)
-    draw_Himage.arc((140, 50, 190, 100), 0, 360, fill = 0)
-    draw_Himage.rectangle((80, 50, 130, 100), fill = 0)
-    draw_Himage.chord((200, 50, 250, 100), 0, 360, fill = 0)
+
+    draw_other.rectangle((20, 20, 50, 50), fill=1)
+
     epd.display(epd.getbuffer(Himage),epd.getbuffer(Other))
-    time.sleep(2)
+
+    time.sleep(5)
 
     logging.info("Clear...")
     epd.init()
